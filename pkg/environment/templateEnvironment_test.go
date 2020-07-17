122058ac161cf08fa04b4255dabb9bb381382699
David Salgado <david@digitalronin.com>
2020-07-17 15:03:16 +0100

    Use `err :=` instead of `var err =`
    This is more idiomatic golang

--- pkg/environment/templateEnvironment_test.go
+++ pkg/environment/templateEnvironment_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestRunningOutsideEnvironmentsWorkingCopy(t *testing.T) {
-	var err = CreateTemplateNamespace(nil, nil)
+	err := CreateTemplateNamespace(nil, nil)
 	if err.Error() != "You are outside cloud-platform-environment repo" {
 		t.Errorf("Unexpected error: %s", err)
 	}
