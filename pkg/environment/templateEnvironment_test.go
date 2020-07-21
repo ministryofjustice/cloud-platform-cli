fadcf1cd8b51cf09be3d3973245cb66465031112
David Salgado <david@digitalronin.com>
2020-07-21 17:35:08 +0100

    Add a test for createNamespaceFiles
    This tests that we can supply templates and values
    and expect the values to be interpolated into the
    templates, and the result written to files defined
    as the outputPath of the templates.

--- pkg/environment/templateEnvironment_test.go
+++ pkg/environment/templateEnvironment_test.go
@@ -1,6 +1,8 @@
 package environment
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -9,4 +11,35 @@ func TestRunningOutsideEnvironmentsWorkingCopy(t *testing.T) {
 	if err.Error() != "This command may only be run from within a working copy of the cloud-platform-environments repository\n" {
 		t.Errorf("Unexpected error: %s", err)
 	}
+}
+
+func TestOutputsInterpolatedTemplateToPath(t *testing.T) {
+	filename := "fixtures/test.txt"
+
+	templates := []*templateFromUrl{
+		{
+			outputPath: filename,
+			content:    "name: {{ .Namespace }}",
+		},
+	}
+
+	values := templateEnvironment{
+		Namespace: "mynamespace",
+	}
+
+	err := createNamespaceFiles(templates, &values)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if string(contents) != "name: mynamespace" {
+		t.Errorf("Expected:\nname: mynamespace\nGot: %s\n", contents)
+	}
+
+	os.Remove(filename)
 }
