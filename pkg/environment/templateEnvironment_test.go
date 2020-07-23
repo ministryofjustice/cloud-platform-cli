6559f72a86616fd8c45247fde28260a68514889b
David Salgado <david@digitalronin.com>
2020-07-23 11:51:06 +0100

    Add a cleanup function after namespace test

--- pkg/environment/templateEnvironment_test.go
+++ pkg/environment/templateEnvironment_test.go
@@ -6,6 +6,32 @@ import (
 	"testing"
 )
 
+func cleanUpNamespacesFolder(namespace string) {
+  namespaceFolder := namespaceBaseFolder+"/"+namespace
+  os.RemoveAll(namespaceBaseFolder)
+  os.Remove(namespaceFolder+"/resources")
+  os.Remove(namespaceFolder)
+  os.Remove(namespaceBaseFolder)
+  os.Remove("namespaces")
+}
+
+func TestCreateNamespace(t *testing.T) {
+  ns := Namespace{ Namespace: "foobar" }
+
+	_, templates := downloadAndInitialiseTemplates(ns.Namespace)
+  if len(templates) != 8 {
+    t.Errorf("Expected 8 templates, got: %d", len(templates))
+  }
+
+  filename := namespaceBaseFolder+"/foobar/00-namespace.yaml"
+  createNamespaceFiles(templates, &ns)
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		t.Errorf("Expected file %s to be created", filename)
+	}
+
+  cleanUpNamespacesFolder("foobar")
+}
+
 func TestRunningOutsideEnvironmentsWorkingCopy(t *testing.T) {
 	err := CreateTemplateNamespace(nil, nil)
 	if err.Error() != "This command may only be run from within a working copy of the cloud-platform-environments repository\n" {
@@ -41,5 +67,6 @@ func TestOutputsInterpolatedTemplateToPath(t *testing.T) {
 		t.Errorf("Expected:\nname: mynamespace\nGot: %s\n", contents)
 	}
 
-	os.Remove(filename)
+  cleanUpNamespacesFolder("mynamespace")
+  os.Remove(filename)
 }
