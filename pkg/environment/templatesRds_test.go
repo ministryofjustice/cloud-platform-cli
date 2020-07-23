087876f414aa92cfd713a6081ba271695b64e4f9
David Salgado <david@digitalronin.com>
2020-07-23 15:40:14 +0100

    Use fileContainsString() in RDS test

--- pkg/environment/templatesRds_test.go
+++ pkg/environment/templatesRds_test.go
@@ -1,9 +1,7 @@
 package environment
 
 import (
-	"io/ioutil"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -16,18 +14,8 @@ func TestCreatesRdsTfFile(t *testing.T) {
 		t.Errorf("Unexpected error: %s", err)
 	}
 
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		t.Errorf("Expected file %s to be created", filename)
-	}
-
-	contents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
 	moduleName := "github.com/ministryofjustice/cloud-platform-terraform-rds-instance"
-	if !(strings.Contains(string(contents), moduleName)) {
-		t.Errorf("Didn't find %s in contents of %s", moduleName, filename)
-	}
+	fileContainsString(t, filename, moduleName)
 
 	os.Remove(filename)
 	os.Remove("resources")
