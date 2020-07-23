edd1d585c8a129912f79e43480b1832ff0fdae5c
David Salgado <david@digitalronin.com>
2020-07-23 15:17:50 +0100

    Use a map of file/string pairs to test files
    This allows us to specify an arbitrary list of 
    filenames and strings those files should contain.

--- pkg/environment/templateEnvironment_test.go
+++ pkg/environment/templateEnvironment_test.go
@@ -65,25 +65,18 @@ func TestCreateNamespace(t *testing.T) {
 		}
 	}
 
-	// test value interpolation
-
-	type testValueInterpolation struct {
-		filename string
-		want     string
+	stringsInFiles := map[string]string{
+		namespaceFile:   "name: foobar",
+		namespaceFile:   "cloud-platform.justice.gov.uk/business-unit: \"My Biz Unit\"",
+		namespaceFile:   "cloud-platform.justice.gov.uk/environment-name: \"envname\"",
+		namespaceFile:   "cloud-platform.justice.gov.uk/application: \"My App\"",
+		namespaceFile:   "cloud-platform.justice.gov.uk/owner: \"Some Team: some-team@digital.justice.gov.uk\"",
+		namespaceFile:   "cloud-platform.justice.gov.uk/source-code: \"https://github.com/ministryofjustice/somerepo\"",
+		rbacFile:        "name: \"github:my-github-team\"",
 	}
 
-	tests := []testValueInterpolation{
-		{filename: namespaceFile, want: "name: foobar"},
-		{filename: namespaceFile, want: "cloud-platform.justice.gov.uk/business-unit: \"My Biz Unit\""},
-		{filename: namespaceFile, want: "cloud-platform.justice.gov.uk/environment-name: \"envname\""},
-		{filename: namespaceFile, want: "cloud-platform.justice.gov.uk/application: \"My App\""},
-		{filename: namespaceFile, want: "cloud-platform.justice.gov.uk/owner: \"Some Team: some-team@digital.justice.gov.uk\""},
-		{filename: namespaceFile, want: "cloud-platform.justice.gov.uk/source-code: \"https://github.com/ministryofjustice/somerepo\""},
-		{filename: rbacFile, want: "name: \"github:my-github-team\""},
-	}
-
-	for _, tc := range tests {
-		fileContainsString(t, tc.filename, tc.want)
+	for filename, searchString := range stringsInFiles {
+		fileContainsString(t, filename, searchString)
 	}
 
 	cleanUpNamespacesFolder("foobar")
