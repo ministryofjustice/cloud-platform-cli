83f2e7f0172d03a640a4664d94a20788208cd465
David Salgado <david@digitalronin.com>
2020-07-23 15:19:43 +0100

    Remove template interpolation test
    This test is redundant now.

--- pkg/environment/templateEnvironment_test.go
+++ pkg/environment/templateEnvironment_test.go
@@ -91,35 +91,3 @@ func TestRunningOutsideEnvironmentsWorkingCopy(t *testing.T) {
 		t.Errorf("Unexpected error: %s", err)
 	}
 }
-
-func TestOutputsInterpolatedTemplateToPath(t *testing.T) {
-	filename := "fixtures/test.txt"
-
-	templates := []*templateFromUrl{
-		{
-			outputPath: filename,
-			content:    "name: {{ .Namespace }}",
-		},
-	}
-
-	values := Namespace{
-		Namespace: "mynamespace",
-	}
-
-	err := createNamespaceFiles(templates, &values)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
-
-	contents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
-
-	if string(contents) != "name: mynamespace" {
-		t.Errorf("Expected:\nname: mynamespace\nGot: %s\n", contents)
-	}
-
-	cleanUpNamespacesFolder("mynamespace")
-	os.Remove(filename)
-}
