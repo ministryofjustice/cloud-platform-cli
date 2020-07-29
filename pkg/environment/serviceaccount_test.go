9d3cbc388edb91974130163d376ef0f263810bea
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-07-29 11:18:03 +0100

    Add test for serviceaccount creation

---
+++ pkg/environment/serviceaccount_test.go
@@ -0,0 +1,24 @@
+package environment
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreateServiceAccountFiles(t *testing.T) {
+	svc := ServiceAccount{
+		Name:      "testName",
+		Namespace: "testNamespace",
+	}
+
+	fileName := "05-serviceaccount.yaml"
+
+	err := createServiceAccountFiles(&svc)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	fileContainsString(t, fileName, svc.Name)
+
+	os.Remove(fileName)
+}
