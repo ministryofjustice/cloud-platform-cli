68aa7ef434a9f2927ee7cb960e70d879f29d2f0b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-07-30 16:34:12 +0100

    Split actions up into three main functions
    There are three actions to the serviceaccount creation:
    - set values
    - pull template
    - write file
    
    I thought this would make more sense to create a separate function for
    each one of these actions.

--- pkg/environment/templateServiceacount.go
+++ pkg/environment/templateServiceacount.go
@@ -36,27 +36,59 @@ func CreateTemplateServiceAccount(name string) error {
 	return nil
 }
 
-// createFile uses the values of a ServiceAccount object to interpolate the serviceaccount
+// createSvcAccountFile uses the values of a ServiceAccount object to interpolate the serviceaccount
 // template, it then creates a file in the current working directory.
-func (v *ServiceAccount) createSvcFile(name string) error {
 func (v *ServiceAccount) createSvcAccFile(name string) error {
+	err := v.setSvcAccValues(name)
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := setSvcAccTemplate()
+	if err != nil {
+		return err
+	}
+
+	err = v.writeSvcAccFile(tmpl)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// setSvcValues takes a ServiceAccount object and populates it with the namespace
+// value (from the 00-namespace.yaml file) and the name argument from cobra.
+func (v *ServiceAccount) setSvcAccValues(name string) error {
 	ns := Namespace{}
 	ns.ReadYaml()
 
 	v.Name = name
 	v.Namespace = ns.Namespace
 
-	templateFile, err := downloadTemplate(templateLocation)
+	return nil
+}
+
+// setSvcTemplate downloads the required template from the environments repository
+// and returns it.
+func setSvcAccTemplate() (string, error) {
+	templateFile, err := downloadTemplate(envTemplateLocation + "/" + svcAccFileName)
 	if err != nil {
-		return (err)
+		return "An error occurred", err
 	}
 
-	tpl := template.Must(template.New("").Parse(templateFile))
+	return templateFile, nil
+}
 
-	f, _ := outputFileWriter(fileName)
-	err = tpl.Execute(f, v)
+// writeSvcAccFile uses the template returned by setSvcTemplate and writes a
+// file to the current working directory.
+func (v *ServiceAccount) writeSvcAccFile(tmpl string) error {
+	tpl := template.Must(template.New("").Parse(tmpl))
+
+	f, _ := outputFileWriter(svcAccFileName)
+	err := tpl.Execute(f, v)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	return nil
