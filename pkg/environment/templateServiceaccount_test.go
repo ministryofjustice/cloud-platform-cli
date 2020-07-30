006cb9cc3cc98da8e62f88e5d8a995db11185848
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-07-30 16:35:37 +0100

    Create dummy namespace resource in test
    To simulate a live environment I had to create a symlink of a namespace
    in the tests working directory.

--- pkg/environment/templateServiceaccount_test.go
+++ pkg/environment/templateServiceaccount_test.go
@@ -5,21 +5,31 @@ import (
 	"testing"
 )
 
+// TestCreateServiceAccountFiles simulates a ServiceAccount object
+// and tests whether the createSvcAccFile function creates a serviceaccount
+// manifest. To do this, the function requires there to be a namespace manifest
+// called 00-namespace.yaml
 func TestCreateServiceAccountFiles(t *testing.T) {
 	svc := ServiceAccount{
 		Name:      "testName",
-		Namespace: "testNamespace",
+		Namespace: "foobar",
 	}
 
-	fileName := "05-serviceaccount.yaml"
+	svcAccFileName := "05-serviceaccount.yaml"
+	svcAccNamespace := "00-namespace.yaml"
 
-	err := svc.createSvcFile(svc.Name)
+	err := os.Link("fixtures/foobar-namespace.yml", svcAccNamespace)
+	if err != nil {
+		t.Error("Error copying file:", err)
+	}
+	err = svc.createSvcAccFile(svc.Name)
 	if err != nil {
 		t.Errorf("Unexpected error: %s", err)
 	}
 
-	fileContainsString(t, fileName, svc.Name)
-	fileContainsString(t, fileName, svc.Namespace)
+	fileContainsString(t, svcAccFileName, svc.Name)
+	fileContainsString(t, svcAccFileName, svc.Namespace)
 
-	os.Remove(fileName)
+	os.Remove(svcAccFileName)
+	os.Remove(svcAccNamespace)
 }
