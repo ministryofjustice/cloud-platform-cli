e8e4c4d3292b14a5e826478df67b9aa4f92d5939
Sablu Miah <sablu.miah@digital.justice.gov.uk>
2020-08-04 17:46:46 +0100

    Update link to use tmpl file from Main branch

--- pkg/environment/templatesS3.go
+++ pkg/environment/templatesS3.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const s3TemplateFile = "https://raw.githubusercontent.com/ministryofjustice/cloud-platform-terraform-s3-bucket/add-tmpl/template/s3.tmpl"
+const s3TemplateFile = "https://raw.githubusercontent.com/ministryofjustice/cloud-platform-terraform-s3-bucket/main/template/s3.tmpl"
 const s3TfFile = "resources/s3.tf"
 
 // CreateTemplateRds creates the terraform files from environment's template folder
