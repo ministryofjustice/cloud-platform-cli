c272fb8ba868e8e9165114098576f00f8a9ba3c2
ash <aisha.nasir@digital.justice.gov.uk>
2020-09-17 15:15:24 +0100

    🐛 allow underscores for slack channels

--- pkg/environment/slackChannelValidator.go
+++ pkg/environment/slackChannelValidator.go
@@ -5,6 +5,6 @@ type slackChannelValidator struct {
 
 func (v *slackChannelValidator) isValid(s string) bool {
 	r := new(regexValidator)
-	r.regex = `^[a-z\-]+$`
+	r.regex = `^[a-z\-_]+$`
 	return r.isValid(s)
 }
