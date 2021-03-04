e1686852535fc9e6b8666ddf98fb813a32593fd5
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-03-04 10:55:45 +0000

    Update version.go

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.8.4"
+var Version = "1.8.5"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
