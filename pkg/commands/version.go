1ae5a79300444b68f6c94e44c41f473ca809c7ef
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-03-11 14:25:56 +0000

    Update versions.go

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.8.5"
+var Version = "1.8.6"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
