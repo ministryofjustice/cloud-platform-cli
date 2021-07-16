cba53c9b281cf7a8d8e37be5b132e60621b29c90
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-16 11:55:12 +0100

    Bump tag to build release

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.9.6"
+var Version = "1.9.7"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
