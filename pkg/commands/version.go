038c72916c743e299e3c8f35d7aeb97df75672c4
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2021-09-03 11:14:06 +0100

    Bump version to 1.11.2

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.11.1"
+var Version = "1.11.2"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
