443763f3b6ac66ceb8d17ccdfe2a809e0e9cc83a
Alejandro Garrido Mota <garridomota@gmail.com>
2021-09-06 20:14:37 +0200

    Updated version number

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.11.2"
+var Version = "1.12.0"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
