36c4c0aaa4053bcd96dace5b5b92e791c4545ab8
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-10-27 13:40:17 +0100

    Allow underscore for github repo

--- pkg/environment/githubUrlValidator.go
+++ pkg/environment/githubUrlValidator.go
@@ -5,6 +5,6 @@ type githubUrlValidator struct {
 
 func (v *githubUrlValidator) isValid(s string) bool {
 	r := new(regexValidator)
-	r.regex = `^https:\/\/github\.com\/[a-z\/\-\.]+$`
+	r.regex = `^https:\/\/github\.com\/[a-z\/\-_\.]+$`
 	return r.isValid(s)
 }
