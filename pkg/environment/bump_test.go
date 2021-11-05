9e2497f9b174472f7fc04d0a623b98dde0346ccb
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-11-05 21:49:24 +0000

    Create good and bad paths for Bump test
    The test runs through a common scenario, mainly testing the Terraform source change.
    
    First we create a test file with an array of bytes. We then pass the new version and the module name (in this case it's "test"). The function should change the "test" version to the string passed. If it doesn't, the `checkModuleChange` will return a false bool and the test will fail.

--- pkg/environment/bump_test.go
+++ pkg/environment/bump_test.go
@@ -1,53 +1,93 @@
 package environment
 
 import (
-	"log"
+	"bytes"
+	"fmt"
 	"os"
 	"testing"
 )
 
-func createTestFile() error {
-	f, err := os.Create("test.tf")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	f.WriteString("module test { source = \"test=0.1.1\" }")
-
-	return nil
-}
-
 func TestBumpModule(t *testing.T) {
-	err := createTestFile()
-	if err != nil {
-		log.Fatalln("Failed to create test Terraform file:", err)
-	}
-
-	defer os.Remove("test.tf")
-
 	type args struct {
-		m string
-		v string
+		moduleName   string
+		newVersion   string
+		checkVersion string
+		file         os.File
 	}
+
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+		name        string
+		args        args
+		wantErr     bool
+		wantSuccess bool
 	}{
 		{
-			name: "Bump module version",
+			name: "Correctly bump a module version",
 			args: args{
-				m: "test",
-				v: "0.1.2",
+				moduleName:   "test",
+				newVersion:   "0.1.2",
+				checkVersion: "0.1.2",
+				file:         createTestFile(),
 			},
-			wantErr: false,
+			wantErr:     false,
+			wantSuccess: true,
+		},
+		{
+			name: "Incorrectly bump a module version",
+			args: args{
+				moduleName:   "test",
+				newVersion:   "0.1.2",
+				checkVersion: "NOTHING",
+				file:         createTestFile(),
+			},
+			wantErr:     false,
+			wantSuccess: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := BumpModule(tt.args.m, tt.args.v); (err != nil) != tt.wantErr {
+			if err := BumpModule(tt.args.moduleName, tt.args.newVersion); (err != nil) != tt.wantErr {
 				t.Errorf("BumpModule() error = %v, wantErr %v", err, tt.wantErr)
 			}
+
+			if checkModuleChange(tt.args.checkVersion, tt.args.file.Name()) != tt.wantSuccess {
+				t.Errorf("BumpModule() checkSourceChange = %v, want %v", checkModuleChange(tt.args.checkVersion, tt.args.file.Name()), tt.wantSuccess)
+			}
+			defer os.Remove(tt.args.file.Name())
 		})
 	}
+}
+
+// createTestFile creates a test file with the given version.
+func createTestFile() os.File {
+	f, err := os.Create("test.tf")
+	if err != nil {
+		fmt.Errorf("Error creating file: %e", err)
+	}
+
+	defer f.Close()
+	f.WriteString("module test { source = \"test=1.0.0\" }")
+
+	return *f
+}
+
+const chunkSize = 64000
+
+// checkModuleChange checks if the file has been changed and contains
+// the string passed to it.
+func checkModuleChange(v, f string) bool {
+	file, err := os.Open(f)
+	if err != nil {
+		fmt.Errorf("Error reading file: %e", err)
+	}
+	defer file.Close()
+
+	b := make([]byte, chunkSize)
+	_, err = file.Read(b)
+
+	if err != nil {
+		fmt.Errorf("Error reading file: %e", err)
+	}
+
+	return bytes.Contains(b, []byte(v))
 }
