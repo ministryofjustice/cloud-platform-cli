768cbf29dc5f4d49cb86edd29d61921b7ab5ba9a
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2021-11-08 13:08:04 +0000

    Fix broken build version - bump to 1.13.2

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.13.1"
+var Version = "1.13.2"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
