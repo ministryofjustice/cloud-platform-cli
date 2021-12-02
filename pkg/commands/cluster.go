a226521de8560d6c02dec263907194aa6cb4bd47
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-12-02 15:13:35 +0000

    feat(cluster): Add clusterDrainCmd struct type and flags to change config

--- pkg/commands/cluster.go
+++ pkg/commands/cluster.go
@@ -5,9 +5,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type clusterDrainCmd struct {
+	Node    string // name of the node to drain
+	Force   bool   // force drain and ignore customer uptime requests
+	DryRun  bool   // don't actually drain the node
+	TimeOut int    // draining a node usually takes around two minutes. If it takes longer than this, it will be cancelled.
+}
+
+var c clusterDrainCmd
+
 func addClusterCmd(topLevel *cobra.Command) {
 	topLevel.AddCommand(clusterCmd)
+
+	// sub cobra commands
 	clusterCmd.AddCommand(clusterDrainNodeCmd)
+
+	// flags
+	clusterDrainNodeCmd.Flags().StringVarP(&c.Node, "node", "n", "", "node to drain")
+	clusterDrainNodeCmd.Flags().BoolVarP(&c.Force, "force", "f", false, "force drain and ignore customer uptime requests")
+	clusterDrainNodeCmd.Flags().BoolVar(&c.DryRun, "dry-run", false, "don't actually drain the node")
+	clusterDrainNodeCmd.Flags().IntVarP(&c.TimeOut, "timeout", "t", 360, "draining a node usually takes around two minutes. If it takes longer than this, it will be cancelled.")
 }
 
 var clusterCmd = &cobra.Command{
@@ -23,4 +40,8 @@ var clusterDrainNodeCmd = &cobra.Command{
 	$ cloud-platform cluster drain-node
 	`),
 	PreRun: upgradeIfNotLatest,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		return nil
+	},
 }
