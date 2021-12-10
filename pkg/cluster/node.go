f2d32441a1e3c17caaaf675efc560cf6eee2d0fd
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-12-10 20:43:55 +0000

    feat(recycle-node): Add recycleNode function and assertions

--- pkg/cluster/node.go
+++ pkg/cluster/node.go
@@ -2,16 +2,19 @@ package cluster
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/ministryofjustice/cloud-platform-environments/pkg/authenticate"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	"k8s.io/kubectl/pkg/drain"
 )
 
 type RecycleNodeOpt struct {
@@ -23,18 +26,6 @@ type RecycleNodeOpt struct {
 	Oldest  bool        // drain the oldest node
 }
 
-func getKubeConfigPath() string {
-	// Set the filepath of the kubeconfig file. This assumes
-	// the user has either the envname set or stores their config file
-	// in the default location.
-	configFile := os.Getenv("KUBECONFIG")
-	if configFile == "" {
-		configFile = filepath.Join(homedir.HomeDir(), ".kube", "config")
-	}
-
-	return configFile
-}
-
 func RecycleNode(opt *RecycleNodeOpt) error {
 	// auth to cluster
 	clientset, err := authenticate.CreateClientFromConfigFile(getKubeConfigPath(), "")
@@ -42,12 +33,23 @@ func RecycleNode(opt *RecycleNodeOpt) error {
 		return err
 	}
 
+	// ensure all nodes are in a ready state
+	err = workerNodesRunning(*clientset)
+	if err != nil {
+		return err
+	}
+
 	// if oldest flag true, check the oldest node is the one we want to drain
 	if opt.Oldest {
-		err = opt.RecycleOldestNode(*clientset)
+		err = opt.getOldestNode(*clientset)
 		if err != nil {
 			return err
 		}
+	}
+
+	// Catch empty node name
+	if opt.Node == "" {
+		return fmt.Errorf("node name is required")
 	}
 
 	return RecycleNodeByName(*clientset, opt)
@@ -61,7 +63,7 @@ func RecycleNode(opt *RecycleNodeOpt) error {
 // delete any stuck pods
 // drain the nodes
 
-func (o *RecycleNodeOpt) RecycleOldestNode(client kubernetes.Clientset) error {
+func (o *RecycleNodeOpt) getOldestNode(client kubernetes.Clientset) error {
 	nodes := client.CoreV1().Nodes()
 
 	// get the oldest node
@@ -85,11 +87,129 @@ func (o *RecycleNodeOpt) RecycleOldestNode(client kubernetes.Clientset) error {
 	return nil
 }
 
-func RecycleNodeByName(client kubernetes.Clientset, opt *RecycleNodeOpt) error {
+func deleteStuckPods(client kubernetes.Clientset, n *v1.Node) error {
+	stuckStates := stuckStates()
+
+	// get the pods on the node
+	pods := client.CoreV1().Pods(n.Namespace)
+	list, err := pods.List(context.Background(), metav1.ListOptions{
+		LabelSelector: "node=" + n.Name,
+	})
+	if err != nil {
+		return err
+	}
+
+	// if there are any pods on the node that are stuck, delete them
+	if len(list.Items) > 0 {
+		for _, pod := range list.Items {
+			for _, state := range stuckStates {
+				if pod.Status.Reason == state {
+					err := pods.Delete(context.Background(), pod.Name, metav1.DeleteOptions{})
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+	}
 	return nil
 }
 
-func getLocalKubeConfig() (*kubernetes.Clientset, error) {
+func drainNode(client kubernetes.Clientset, node *v1.Node) error {
+	helper := &drain.Helper{
+		// Client:              client,
+		Force:               true,
+		GracePeriodSeconds:  -1,
+		IgnoreAllDaemonSets: true,
+		Out:                 os.Stdout,
+		ErrOut:              os.Stdout,
+		// We want to proceed even when pods are using emptyDir volumes
+		DeleteEmptyDirData: true,
+		Timeout:            time.Duration(120) * time.Second,
+	}
+
+	if err := drain.RunCordonOrUncordon(helper, node, true); err != nil {
+		return fmt.Errorf("error cordoning node: %v", err)
+	}
+
+	if err := drain.RunNodeDrain(helper, node.Name); err != nil {
+		return fmt.Errorf("error draining node: %v", err)
+	}
+
+	return nil
+}
+
+func RecycleNodeByName(client kubernetes.Clientset, opt *RecycleNodeOpt) error {
+
+	// check the node exists
+	nodes := client.CoreV1().Nodes()
+
+	node, err := nodes.Get(context.Background(), opt.Node, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	// delete any stuck pods
+	err = deleteStuckPods(client, node)
+	if err != nil {
+		return err
+	}
+	// ensure the node is labeled
+	// if !node.Labels["cloud-platform-recycle-nodes"] {
+	// 	return fmt.Errorf("node %s is not labeled for recycling", opt.Node)
+	// }
+
+	// cordon the node
+	err = drainNode(client, node)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// workerNodesRunning takes a client-go argument and checks if all nodes reported by
+// `kubectl get nodes` report in a "Ready" state. If a node reports anything
+// other than "Ready" the function will error. If all nodes
+// report "Ready" then it'll return a nil.
+//
+// This acts as a validation to ensure we can start recycling nodes.
+// You wouldn't want to start recycling on an unhealthy cluster.
+func workerNodesRunning(client kubernetes.Clientset) error {
+	nodes := client.CoreV1().Nodes()
+
+	list, err := nodes.List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	var workingNodes int
+	for _, node := range list.Items {
+		if node.Status.Conditions[0].Type == "Ready" && node.Status.Conditions[0].Status != "True" {
+			return fmt.Errorf("node %s is not ready", node.Name)
+		}
+		workingNodes++
+	}
+
+	// if all nodes report a "Ready" state then approve
+	if workingNodes != len(list.Items) {
+		return fmt.Errorf("all nodes check failed to report a ready state. Please ensure all nodes are Ready")
+	}
+
+	return nil
+}
+
+func stuckStates() []string {
+	return []string{
+		"Pending",
+		"Scheduling",
+		"Unschedulable",
+		"ImagePullBackOff",
+		"CrashLoopBackOff",
+		"Unknown",
+	}
+}
+
+func getKubeConfigPath() string {
 	// Set the filepath of the kubeconfig file. This assumes
 	// the user has either the envname set or stores their config file
 	// in the default location.
@@ -98,16 +218,5 @@ func getLocalKubeConfig() (*kubernetes.Clientset, error) {
 		configFile = filepath.Join(homedir.HomeDir(), ".kube", "config")
 	}
 
-	// Build the Kubernetes client using the default context (user set).
-	config, err := clientcmd.BuildConfigFromFlags("", configFile)
-	if err != nil {
-		return nil, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientset, nil
+	return configFile
 }
