06cc8b7a626553e4397130d9a0065a00e4806cf5
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-01-20 13:59:12 +0000

    docs: :memo: Add documentation for Go docs

---
+++ pkg/recycle/document.go
@@ -0,0 +1,5 @@
+/*
+Package recycle provides options and functions to help with the process of
+recycling Kubernetes objects, such as nodes, pods, and services.
+*/
+package recycle // import "github.com/ministryofjustice/cloud-platform-cli/cmd/recycle"
