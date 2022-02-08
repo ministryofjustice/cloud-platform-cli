f3ee1ae51dfccc17b464849a85d3c8e21e0d52ff
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-02-08 11:47:26 +0000

    Update version to match to the latest release

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 // This MUST match the number of the latest release on github
-var Version = "1.14.3"
+var Version = "1.14.4"
 
 const owner = "ministryofjustice"
 const repoName = "cloud-platform-cli"
