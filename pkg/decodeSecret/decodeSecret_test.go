ddf6fe3166efc33d0c93f677837fec0e5f09acb1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-08-22 13:13:23 +0100

    fix(decode-secret): add "raw" flag to allow user to print unicode
    This commit connects to https://github.com/ministryofjustice/cloud-platform-cli/issues/103 and offers mitigation (not a fix) to a problem where a secret contains a unicode char. We use the stdlib json package to handle a "prettyprint" back to the end user. When doing so, we marshal a raw string into json, which handles UTF-8 only (please see here: https://pkg.go.dev/encoding/json#Marshal). This commit gives the option to specify a `--raw` or `-r` flag to the subcommand and print before marshaling. This loops over all secret values, decodes the base64 and then prints them raw with no formatting applied.

--- pkg/decodeSecret/decodeSecret_test.go
+++ pkg/decodeSecret/decodeSecret_test.go
@@ -1,6 +1,7 @@
 package decodeSecret
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -17,7 +18,7 @@ func TestDecodeSecret(t *testing.T) {
 `
 
 	sd := secretDecoder{}
-	actual, err := sd.processJson(jsn)
+	actual, err := sd.processJson(jsn, false)
 	if err != nil || actual != strings.TrimSpace(expected) {
 		t.Errorf("Expected:\n%s\nGot:\n%s\n", expected, actual)
 	}
@@ -34,7 +35,7 @@ func TestBadBase64(t *testing.T) {
 }
 `
 	sd := secretDecoder{}
-	actual, err := sd.processJson(jsn)
+	actual, err := sd.processJson(jsn, false)
 	if err != nil || actual != strings.TrimSpace(expected) {
 		t.Errorf("Expected:\n%s\nGot:\n%s\n", expected, actual)
 	}
@@ -42,7 +43,7 @@ func TestBadBase64(t *testing.T) {
 
 func TestNoSuchSecret(t *testing.T) {
 	sd := secretDecoder{}
-	_, err := sd.processJson("")
+	_, err := sd.processJson("", false)
 	if err == nil {
 		t.Errorf("Expected an error")
 	}
@@ -52,11 +53,22 @@ func TestRecordsAwsSecrets(t *testing.T) {
 	// jsn := `{ "data": { "access_key_id": "myaccesskey", "secret_access_key": "mysecretkey" } }`
 	jsn := `{ "data": { "access_key_id": "bXlhY2Nlc3NrZXk=", "secret_access_key": "bXlzZWNyZXRrZXk=" } }`
 	sd := secretDecoder{}
-	_, err := sd.processJson(jsn)
+	_, err := sd.processJson(jsn, false)
 	if err != nil || sd.AccessKeyID != "myaccesskey" {
 		t.Errorf("Expected:\n%s\nGot:\n%s\n", "myaccesskey", sd.AccessKeyID)
 	}
 	if err != nil || sd.SecretAccessKey != "mysecretkey" {
 		t.Errorf("Expected:\n%s\nGot:\n%s\n", "mysecretkey", sd.SecretAccessKey)
+	}
+}
+
+func TestOutputtingUnicodeSecrets(t *testing.T) {
+	jsn := `{ "data": { "key1": "dGVzdFVuaWNvZGU8Pj9A"} }`
+
+	expected := fmt.Sprintf("key1: testUnicode<>?@\n")
+	sd := secretDecoder{}
+	actual, err := sd.processJson(jsn, true)
+	if err != nil || actual != expected {
+		t.Errorf("\nExpected:%s\nGot:%s\n", expected, actual)
 	}
 }
