6bc0ee2daff588ae4f3e8263d827d599baee7386
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2022-09-14 09:22:48 +0100

    feat(Go): Adding tests for each scanned word

--- pkg/terraform/logger_test.go
+++ pkg/terraform/logger_test.go
@@ -4,37 +4,53 @@ import "testing"
 
 func TestCmdOutput_redacted(t *testing.T) {
 	type fields struct {
-		Stdout   string
-		Stderr   string
-		ExitCode int
+		Stdout string
 	}
 	tests := []struct {
 		name   string
 		fields fields
 	}{
 		{
-			name: "Redacted Content",
+			name: "Redacted Password Content",
 			fields: fields{
-				Stdout:   "password: 1234567890",
-				Stderr:   "",
-				ExitCode: 0,
+				Stdout: "password: 1234567890",
+			},
+		},
+		{
+			name: "Redacted Sercet Content",
+			fields: fields{
+				Stdout: "secret: 1234567890",
+			},
+		},
+		{
+			name: "Redacted Token Content",
+			fields: fields{
+				Stdout: "token: 1234567890",
+			},
+		},
+		{
+			name: "Redacted Key Content",
+			fields: fields{
+				Stdout: "key: 1234567890",
+			},
+		},
+		{
+			name: "Redacted Webhook Content",
+			fields: fields{
+				Stdout: "https://hooks.slack.com",
 			},
 		},
 		{
 			name: "Unredacted Content",
 			fields: fields{
-				Stdout:   "This is a test",
-				Stderr:   "",
-				ExitCode: 0,
+				Stdout: "This test should not be redacted",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			o := &CmdOutput{
-				Stdout:   tt.fields.Stdout,
-				Stderr:   tt.fields.Stderr,
-				ExitCode: tt.fields.ExitCode,
+				Stdout: tt.fields.Stdout,
 			}
 			o.redacted()
 		})
