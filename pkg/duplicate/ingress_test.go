9fd51fc7898f25264bcd727041deadd200ea3e69
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-09-16 16:39:21 +0100

    test(duplicate): move to duplicate_test package
    this will allow us to get a better feel for how the package is called i.e. from the commands package.

--- pkg/duplicate/ingress_test.go
+++ pkg/duplicate/ingress_test.go
@@ -1,10 +1,11 @@
-package duplicate
+package duplicate_test
 
 import (
 	"context"
 	"reflect"
 	"testing"
 
+	"github.com/ministryofjustice/cloud-platform-cli/pkg/duplicate"
 	"github.com/ministryofjustice/cloud-platform-go-library/client"
 	"github.com/ministryofjustice/cloud-platform-go-library/mock"
 	v1 "k8s.io/api/networking/v1"
@@ -44,7 +45,7 @@ func TestNewIngress(t *testing.T) {
 	}
 
 	// Pass good ingress to NewIngress. Expected to return nil error.
-	dup, err := NewIngress(&fakeClient.Clientset, "Namespace2", "test")
+	dup, err := duplicate.NewIngress(&fakeClient.Clientset, "Namespace2", "test")
 	if !reflect.DeepEqual(ingress.Name, dup.Resource.Name) {
 		t.Errorf("Expected %v, got %v", ingress, dup.Resource.Name)
 	}
@@ -53,7 +54,7 @@ func TestNewIngress(t *testing.T) {
 	}
 
 	// Pass bad ingress to NewIngress. Expected to return error.
-	_, err = NewIngress(&fakeClient.Clientset, "Namespace1", "bad")
+	_, err = duplicate.NewIngress(&fakeClient.Clientset, "Namespace1", "bad")
 	if err == nil {
 		t.Errorf("Expected %v, got %v", "error", nil)
 	}
@@ -75,7 +76,7 @@ func TestBadCreateDuplicate(t *testing.T) {
 	}
 
 	// Pass bad ingress to CreateDuplicate. Expected to return error.
-	dup := &Ingress{
+	dup := &duplicate.Ingress{
 		Resource:  badIngress,
 		Clientset: &fakeClient.Clientset,
 	}
@@ -145,7 +146,7 @@ func TestGoodCreateDuplicate(t *testing.T) {
 	}
 
 	// Pass good ingress to CreateDuplicate. Expected to return nil error.
-	goodDup := &Ingress{
+	goodDup := &duplicate.Ingress{
 		Resource:  goodIngress,
 		Clientset: &fakeClient.Clientset,
 	}
@@ -167,117 +168,4 @@ func TestGoodCreateDuplicate(t *testing.T) {
 			t.Errorf("Error deleting ingress: %v", err)
 		}
 	}()
-}
-
-func Test_copyAndChangeIngress(t *testing.T) {
-	type args struct {
-		inIngress *v1.Ingress
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *v1.Ingress
-		wantErr bool
-	}{
-		{
-			name: "Change name, set-identifier and Hosts",
-			args: args{
-				inIngress: &v1.Ingress{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "ing-01",
-						Namespace: "ns-01",
-						Annotations: map[string]string{
-							"external-dns.alpha.kubernetes.io/aws-weight":     "100",
-							"external-dns.alpha.kubernetes.io/set-identifier": "ing-01-ns-01-green",
-						},
-					},
-					Spec: v1.IngressSpec{
-						Rules: []v1.IngressRule{
-							{
-								Host: "example-ingress.domain.com",
-								IngressRuleValue: v1.IngressRuleValue{
-									HTTP: &v1.HTTPIngressRuleValue{
-										Paths: []v1.HTTPIngressPath{
-											{
-												Path:     "/",
-												PathType: func() *v1.PathType { p := v1.PathTypeImplementationSpecific; return &p }(),
-												Backend: v1.IngressBackend{
-													Service: &v1.IngressServiceBackend{
-														Name: "svc-01",
-														Port: v1.ServiceBackendPort{
-															Number: 1234,
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						TLS: []v1.IngressTLS{
-							{
-								Hosts: []string{"example-ingress.domain.com"},
-							},
-						},
-					},
-					Status: v1.IngressStatus{},
-				},
-			},
-			want: &v1.Ingress{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "ing-01-duplicate",
-					Namespace: "ns-01",
-					Annotations: map[string]string{
-						"external-dns.alpha.kubernetes.io/aws-weight":     "100",
-						"external-dns.alpha.kubernetes.io/set-identifier": "ing-01-duplicate-ns-01-green",
-					},
-				},
-				Spec: v1.IngressSpec{
-					Rules: []v1.IngressRule{
-						{
-							Host: "example-ingress-duplicate.domain.com",
-							IngressRuleValue: v1.IngressRuleValue{
-								HTTP: &v1.HTTPIngressRuleValue{
-									Paths: []v1.HTTPIngressPath{
-										{
-											Path:     "/",
-											PathType: func() *v1.PathType { p := v1.PathTypeImplementationSpecific; return &p }(),
-											Backend: v1.IngressBackend{
-												Service: &v1.IngressServiceBackend{
-													Name: "svc-01",
-													Port: v1.ServiceBackendPort{
-														Number: 1234,
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-					TLS: []v1.IngressTLS{
-						{
-							Hosts: []string{"example-ingress-duplicate.domain.com"},
-						},
-					},
-				},
-				Status: v1.IngressStatus{},
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := copyAndChangeIngress(tt.args.inIngress)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("copyAndChangeIngress() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("copyAndChangeIngress() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
