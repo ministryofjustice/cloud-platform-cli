76bad85a0f895bdbece5436f4bb663a741a7d94a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-09-22 11:39:28 +0100

    refactor(cluster): remove globals from package and move flags

--- pkg/commands/cluster.go
+++ pkg/commands/cluster.go
@@ -13,28 +13,11 @@ import (
 	"k8s.io/client-go/util/homedir"
 )
 
-var opt recycle.Options
-
-var awsSecret, awsAccessKey, awsProfile string
-
 func addClusterCmd(topLevel *cobra.Command) {
 	topLevel.AddCommand(clusterCmd)
 
 	// sub cobra commands
 	clusterCmd.AddCommand(clusterRecycleNodeCmd)
-
-	// recycle node flags
-	clusterRecycleNodeCmd.Flags().StringVarP(&opt.ResourceName, "name", "n", "", "name of the resource to recycle")
-	clusterRecycleNodeCmd.Flags().BoolVarP(&opt.Force, "force", "f", true, "force the pods to drain")
-	clusterRecycleNodeCmd.Flags().BoolVarP(&opt.IgnoreLabel, "ignore-label", "i", false, "whether to ignore the labels on the resource")
-	clusterRecycleNodeCmd.Flags().IntVarP(&opt.TimeOut, "timeout", "t", 360, "amount of time to wait for the drain command to complete")
-	clusterRecycleNodeCmd.Flags().BoolVar(&opt.Oldest, "oldest", false, "whether to recycle the oldest node")
-	clusterRecycleNodeCmd.Flags().StringVar(&opt.KubecfgPath, "kubecfg", filepath.Join(homedir.HomeDir(), ".kube", "config"), "path to kubeconfig file")
-	clusterRecycleNodeCmd.Flags().StringVar(&awsAccessKey, "aws-access-key", os.Getenv("AWS_ACCESS_KEY_ID"), "aws access key to use")
-	clusterRecycleNodeCmd.Flags().StringVar(&awsSecret, "aws-secret-key", os.Getenv("AWS_SECRET_ACCESS_KEY"), "aws secret to use")
-	clusterRecycleNodeCmd.Flags().StringVar(&awsProfile, "aws-profile", os.Getenv("AWS_PROFILE"), "aws profile to use")
-	clusterRecycleNodeCmd.Flags().StringVar(&opt.AwsRegion, "aws-region", "eu-west-2", "aws region to use")
-	clusterRecycleNodeCmd.Flags().BoolVar(&opt.Debug, "debug", false, "enable debug logging")
 }
 
 var clusterCmd = &cobra.Command{
@@ -54,6 +37,22 @@ var clusterRecycleNodeCmd = &cobra.Command{
 		contextLogger := log.WithFields(log.Fields{"subcommand": "recycle-node"})
 		// Check for missing name argument. You must define either a resource
 		// or specify the --oldest flag.
+		// recycle node flags
+		var opt recycle.Options
+
+		var awsSecret, awsAccessKey, awsProfile string
+		cmd.Flags().StringVarP(&opt.ResourceName, "name", "n", "", "name of the resource to recycle")
+		cmd.Flags().BoolVarP(&opt.Force, "force", "f", true, "force the pods to drain")
+		cmd.Flags().BoolVarP(&opt.IgnoreLabel, "ignore-label", "i", false, "whether to ignore the labels on the resource")
+		cmd.Flags().IntVarP(&opt.TimeOut, "timeout", "t", 360, "amount of time to wait for the drain command to complete")
+		cmd.Flags().BoolVar(&opt.Oldest, "oldest", false, "whether to recycle the oldest node")
+		cmd.Flags().StringVar(&opt.KubecfgPath, "kubecfg", filepath.Join(homedir.HomeDir(), ".kube", "config"), "path to kubeconfig file")
+		cmd.Flags().StringVar(&awsAccessKey, "aws-access-key", os.Getenv("AWS_ACCESS_KEY_ID"), "aws access key to use")
+		cmd.Flags().StringVar(&awsSecret, "aws-secret-key", os.Getenv("AWS_SECRET_ACCESS_KEY"), "aws secret to use")
+		cmd.Flags().StringVar(&awsProfile, "aws-profile", os.Getenv("AWS_PROFILE"), "aws profile to use")
+		cmd.Flags().StringVar(&opt.AwsRegion, "aws-region", "eu-west-2", "aws region to use")
+		cmd.Flags().BoolVar(&opt.Debug, "debug", false, "enable debug logging")
+
 		if opt.ResourceName == "" && !opt.Oldest {
 			contextLogger.Fatal("--name or --oldest is required")
 		}
