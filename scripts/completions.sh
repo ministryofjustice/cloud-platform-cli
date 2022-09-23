c5bd83d3cbd7d48ee116b79f1936e5233aa938f5
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2022-09-23 18:08:11 +0100

    build(completions): change binary file location

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run ./cmd/cloud-platform/main.go completion "$sh" >"completions/cloud-platform.$sh"
+	go run ./main.go completion "$sh" >"completions/cloud-platform.$sh"
 done
