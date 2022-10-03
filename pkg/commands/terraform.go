a0e6b0e2a17528c6c59dbbb66f3414cecf4d7a7a
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-10-03 14:06:10 +0100

    Use terraform package for check-divergence

--- pkg/commands/terraform.go
+++ pkg/commands/terraform.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"context"
+	"fmt"
 	"strings"
 
+	"github.com/hashicorp/terraform-exec/tfexec"
 	terraform "github.com/ministryofjustice/cloud-platform-cli/pkg/terraform"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -11,7 +14,7 @@ import (
 )
 
 func addTerraformCmd(topLevel *cobra.Command) {
-	var options terraform.Commander
+	var tf terraform.TerraformCLIConfig
 
 	rootCmd := &cobra.Command{
 		Use:    "terraform",
@@ -27,78 +30,33 @@ func addTerraformCmd(topLevel *cobra.Command) {
 			contextLogger := log.WithFields(log.Fields{"subcommand": "check-divergence"})
 
 			contextLogger.Info("Executing terraform plan, if there is a drift this program execution will fail")
-			err := options.CheckDivergence()
-			if err != nil {
-				contextLogger.Fatal("Error executing plan, either an error or a divergence")
+
+			if err := checkDivergence(&tf); err != nil {
+				contextLogger.Fatal(err)
 			}
 		},
 	}
 
-	apply := &cobra.Command{
-		Use:    "apply",
-		Short:  `Execute terraform apply.`,
-		PreRun: upgradeIfNotLatest,
-		Run: func(cmd *cobra.Command, args []string) {
-			contextLogger := log.WithFields(log.Fields{"subcommand": "apply"})
-
-			if options.BulkTfPaths == "" {
-				contextLogger.Info("Executing terraform apply")
-				err := options.Apply()
-				if err != nil {
-					contextLogger.Fatal("Error executing terraform apply - check the outputs")
-				}
-			} else {
-				err := options.BulkApply()
-				if err != nil {
-					contextLogger.Fatal(err)
-				}
-
-			}
-		},
-	}
-
-	plan := &cobra.Command{
-		Use:    "plan",
-		Short:  `Execute terraform plan.`,
-		PreRun: upgradeIfNotLatest,
-		Run: func(cmd *cobra.Command, args []string) {
-			contextLogger := log.WithFields(log.Fields{"subcommand": "plan"})
-
-			if options.BulkTfPaths == "" {
-				contextLogger.Info("Executing terraform plan")
-				err := options.Plan()
-				if err != nil {
-					contextLogger.Fatal("Error executing terraform plan - check the outputs")
-				}
-			} else {
-				err := options.BulkPlan()
-				if err != nil {
-					contextLogger.Fatal(err)
-				}
-
-			}
-		},
-	}
-
-	addCommonFlags(checkDivergence, &options)
-	addCommonFlags(apply, &options)
-	addCommonFlags(plan, &options)
-	rootCmd.AddCommand(plan)
-	rootCmd.AddCommand(apply)
+	addCommonFlags(checkDivergence, &tf)
 	rootCmd.AddCommand(checkDivergence)
 	topLevel.AddCommand(rootCmd)
 }
 
-func addCommonFlags(cmd *cobra.Command, o *terraform.Commander) {
+func addCommonFlags(cmd *cobra.Command, tf *terraform.TerraformCLIConfig) {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
-	cmd.PersistentFlags().StringVarP(&o.AccessKeyID, "aws-access-key-id", "", "", "Access key id of service account to be used by terraform")
-	cmd.PersistentFlags().StringVarP(&o.SecretAccessKey, "aws-secret-access-key", "", "", "Secret access key of service account to be used by terraform")
-	cmd.PersistentFlags().StringVarP(&o.Workspace, "workspace", "w", "default", "Default workspace where terraform is going to be executed")
-	cmd.PersistentFlags().BoolVarP(&o.DisplayTfOutput, "display-tf-output", "d", true, "Display or not terraform plan output")
-	cmd.PersistentFlags().StringVarP(&o.VarFile, "var-file", "v", "", "tfvar to be used by terraform")
-	cmd.PersistentFlags().StringVar(&o.BulkTfPaths, "dirs-file", "", "Required for bulk-plans, file path which holds directories where terraform plan is going to be executed")
+	var varFile string
+
+	cmd.PersistentFlags().StringVarP(&awsAccessKey, "aws-access-key-id", "", "", "Access key id of service account to be used by terraform")
+	cmd.PersistentFlags().StringVarP(&awsSecret, "aws-secret-access-key", "", "", "Secret access key of service account to be used by terraform")
+	cmd.PersistentFlags().StringVarP(&awsRegion, "aws-region", "", "", "[required] aws region to use")
+	cmd.PersistentFlags().StringVarP(&tf.Workspace, "workspace", "w", "default", "Default workspace where terraform is going to be executed")
+	cmd.PersistentFlags().StringVarP(&varFile, "var-file", "v", "", "tfvar to be used by terraform")
+
+	planOptions := make([]tfexec.PlanOption, 0)
+	planOptions = append(planOptions, tfexec.VarFile(varFile))
+	tf.PlanVars = planOptions
 
 	_ = cmd.MarkPersistentFlagRequired("aws-access-key-id")
 	_ = cmd.MarkPersistentFlagRequired("aws-secret-access-key")
@@ -108,4 +66,27 @@ func addCommonFlags(cmd *cobra.Command, o *terraform.Commander) {
 			_ = cmd.PersistentFlags().Set(f.Name, viper.GetString(f.Name))
 		}
 	})
+}
+
+func checkDivergence(tf *terraform.TerraformCLIConfig) error {
+	terraform, error := terraform.NewTerraformCLI(tf)
+	var diff = false
+	if error != nil {
+		return error
+	}
+
+	err := terraform.Init(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to init terraform: %w", err)
+	}
+
+	if diff, err = terraform.Plan(context.Background()); err != nil {
+		return fmt.Errorf("failed to plan terraform: %w", err)
+	}
+
+	if diff {
+		return fmt.Errorf("There is a drift when executing terraform plan")
+	}
+
+	return nil
 }
--- pkg/terraform/terraform.go
+++ pkg/terraform/terraform.go
@@ -6,15 +6,10 @@
 package terraform
 
 import (
-	"bytes"
 	"context"
 	"errors"
-	"fmt"
-	"os/exec"
 	"path/filepath"
 	"regexp"
-	"strings"
-	"syscall"
 
 	"github.com/hashicorp/go-version"
 	install "github.com/hashicorp/hc-install"
@@ -40,6 +35,8 @@ type TerraformCLI struct {
 	logger     log.Logger
 	// Apply allows you to group apply options passed to Terraform.
 	applyVars []tfexec.ApplyOption
+	// Apply allows you to group apply options passed to Terraform.
+	planVars []tfexec.PlanOption
 	// Init allows you to group init options passed to Terraform.
 	initVars []tfexec.InitOption
 }
@@ -52,22 +49,12 @@ type TerraformCLIConfig struct {
 	// Apply allows you to group apply options passed to Terraform.
 	ApplyVars []tfexec.ApplyOption
 	// Init allows you to group init options passed to Terraform.
+	PlanVars []tfexec.PlanOption
+	// Init allows you to group init options passed to Terraform.
 	InitVars []tfexec.InitOption
 	// Version is the version of Terraform to use.
 	Version string
 	// ExecPath is the path to the Terraform executable.
-}
-
-// Commander struct holds all data required to execute terraform.
-type Commander struct {
-	cmdDir          string
-	cmdEnv          []string
-	AccessKeyID     string
-	SecretAccessKey string
-	Workspace       string
-	VarFile         string
-	DisplayTfOutput bool
-	BulkTfPaths     string
 }
 
 // NewTerraformCLI creates a terraform-exec client and configures and
@@ -117,6 +104,7 @@ func NewTerraformCLI(config *TerraformCLIConfig) (*TerraformCLI, error) {
 		workingDir: config.WorkingDir,
 		workspace:  config.Workspace,
 		applyVars:  config.ApplyVars,
+		planVars:   config.PlanVars,
 		initVars:   config.InitVars,
 	}
 
@@ -176,251 +164,10 @@ func (t *TerraformCLI) Apply(ctx context.Context) error {
 
 // Plan executes the cli command `terraform plan` for a given workspace
 func (t *TerraformCLI) Plan(ctx context.Context) (bool, error) {
-	return t.tf.Plan(ctx)
+	return t.tf.Plan(ctx, t.planVars...)
 }
 
 // Plan executes the cli command `terraform plan` for a given workspace
 func (t *TerraformCLI) Output(ctx context.Context) (map[string]tfexec.OutputMeta, error) {
 	return t.tf.Output(ctx)
-}
-
-// Terraform creates terraform command to be executed
-func (s *Commander) Terraform(args ...string) (*CmdOutput, error) {
-	var stdoutBuf bytes.Buffer
-	var stderrBuf bytes.Buffer
-	var exitCode int
-	var err error
-
-	contextLogger := log.WithFields(log.Fields{
-		"err":    err,
-		"stdout": stdoutBuf.String(),
-		"stderr": stderrBuf.String(),
-		"dir":    s.cmdDir,
-	})
-
-	cmd := exec.Command("terraform", args...)
-
-	if s.cmdDir != "" {
-		cmd.Dir = s.cmdDir
-	}
-
-	if s.cmdEnv != nil {
-		cmd.Env = s.cmdEnv
-	}
-
-	cmd.Stdout = &stdoutBuf
-	cmd.Stderr = &stderrBuf
-
-	err = cmd.Run()
-	if err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			ws := exitError.Sys().(syscall.WaitStatus)
-			exitCode = ws.ExitStatus()
-		}
-		contextLogger.Error("cmd.Run() failed with:")
-		contextLogger.Error(cmd.Stderr)
-
-		cmdOutput := CmdOutput{
-			Stdout:   stdoutBuf.String(),
-			Stderr:   stderrBuf.String(),
-			ExitCode: exitCode,
-		}
-		return &cmdOutput, err
-	} else {
-		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
-		exitCode = ws.ExitStatus()
-	}
-
-	cmdOutput := CmdOutput{
-		Stdout:   stdoutBuf.String(),
-		Stderr:   stderrBuf.String(),
-		ExitCode: exitCode,
-	}
-
-	if cmdOutput.ExitCode != 0 {
-		return &cmdOutput, err
-	} else {
-		return &cmdOutput, nil
-	}
-}
-
-// Init executes terraform init.
-func (s *Commander) Init(p bool) error {
-	output, err := s.Terraform("init")
-	if err != nil {
-		log.Error(output.Stderr)
-		return err
-	}
-
-	if p {
-		log.Info(output.Stdout)
-	}
-
-	return nil
-}
-
-// SelectWs is used to select certain workspace.
-func (s *Commander) SelectWs(ws string) error {
-	output, err := s.Terraform("workspace", "select", ws)
-	if err != nil {
-		return err
-	}
-
-	log.Info(output.Stdout)
-
-	return nil
-}
-
-// CheckDivergence check that there are not changes within certain state, if there are
-// it will return non-zero and pipeline will fail.
-func (s *Commander) CheckDivergence() error {
-	err := s.Init(true)
-	if err != nil {
-		return err
-	}
-
-	err = s.SelectWs(s.Workspace)
-	if err != nil {
-		return err
-	}
-
-	var cmd []string
-
-	// Check if user provided a terraform var-file.
-	if s.VarFile != "" {
-		cmd = []string{fmt.Sprintf("-var-file=%s", s.VarFile)}
-	}
-
-	arg := []string{
-		"plan",
-		"-no-color",
-		"-detailed-exitcode",
-	}
-
-	arg = append(arg, cmd...)
-
-	output, err := s.Terraform(arg...)
-	if err != nil {
-		// there is a drift and hence the cmd returns err with exitcode 2
-		if output.ExitCode == 2 {
-			if s.DisplayTfOutput && output != nil {
-				output.redacted()
-			}
-		}
-		return err
-	}
-
-	if s.DisplayTfOutput && output != nil {
-		output.redacted()
-	}
-
-	if output.ExitCode == 0 {
-		return nil
-	}
-	return err
-}
-
-// Apply executes terraform apply.
-func (s *Commander) Apply() error {
-	err := s.Init(true)
-	if err != nil {
-		return err
-	}
-
-	err = s.SelectWs(s.Workspace)
-	if err != nil {
-		return err
-	}
-
-	var cmd []string
-
-	// Check if user provided a terraform var-file
-	if s.VarFile != "" {
-		cmd = []string{fmt.Sprintf("-var-file=%s", s.VarFile)}
-	}
-
-	arg := []string{
-		"apply",
-		"-no-color",
-		"-auto-approve",
-	}
-
-	arg = append(arg, cmd...)
-
-	output, err := s.Terraform(arg...)
-	if err != nil {
-		return err
-	}
-
-	if s.DisplayTfOutput && output != nil {
-		output.redacted()
-	}
-
-	if output.ExitCode == 0 {
-		return nil
-	}
-
-	return err
-}
-
-// Plan executes terraform plan
-func (s *Commander) Plan() error {
-	err := s.Init(false)
-	if err != nil {
-		return err
-	}
-
-	err = s.SelectWs(s.Workspace)
-	if err != nil {
-		return err
-	}
-
-	var cmd []string
-
-	// Check if user provided a terraform var-file
-	if s.VarFile != "" {
-		cmd = []string{fmt.Sprintf("-var-file=%s", s.VarFile)}
-	}
-
-	arg := []string{
-		"plan",
-		"-no-color",
-	}
-
-	arg = append(arg, cmd...)
-
-	output, err := s.Terraform(arg...)
-	if err != nil {
-		return err
-	}
-
-	if s.DisplayTfOutput && output != nil {
-		output.redacted()
-	}
-
-	if output.ExitCode == 0 {
-		return nil
-	}
-
-	return err
-}
-
-// Workspaces return the workspaces within the state.
-func (c *Commander) workspaces() ([]string, error) {
-	arg := []string{
-		"workspace",
-		"list",
-	}
-
-	output, err := c.Terraform(arg...)
-	if err != nil {
-		return nil, err
-	}
-
-	ws := strings.Split(output.Stdout, " ")
-	for i := range ws {
-		ws[i] = strings.TrimSpace(ws[i])
-	}
-
-	return ws, nil
 }
