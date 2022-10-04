4ec8a039cc5dbef153988dc58874aef34c7a1741
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-10-04 11:15:32 +0100

    feat(cluster): add new sub-command for creating a cluster
    the following change is a shell for the eventual command `cloud-platform cluster create`. This command will traverse the terraform directories in the `cloud-platform-infrastructure` repository and perform the necessary `init/plan/apply` to give the caller a "cloud-platform ready" cluster.

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -9,12 +9,32 @@ import (
 
 // Cluster represents useful values and object in a Kubernetes cluster
 type Cluster struct {
-	Name       string
-	Nodes      []v1.Node
-	Pods       []v1.Pod
+	// Name is the name of the cluster object
+	Name string
+	// VpcId is the ID of the VPC the cluster is in. This is usually the same
+	// as the cluster name and is rarely used.
+	VpcId string
+	// Nodes contains a slice of nodes in the cluster. These are Kubernetes objects
+	// so they contain a lot of information.
+	Nodes []v1.Node
+	// OldestNode is the node that was created first in the cluster. It is usually the first
+	// node to be restarted when a cluster is updated.
 	OldestNode v1.Node
+	// NewestNode is the node that was created last in the cluster.
 	NewestNode v1.Node
-	StuckPods  []v1.Pod
+
+	// Pods contains a slice of pods in the cluster.
+	Pods []v1.Pod
+	// StuckPods contains a slice of pods that are stuck in the cluster. Stuck in this case refers to
+	// pods that are in a non-running state for more than 5 minutes.
+	StuckPods []v1.Pod
+
+	// Namespaces contains a slice of namespaces in the cluster.
+	Namespaces v1.NamespaceList
+
+	// HealthStatus is used to define the health of a cluster. This is manually set by the caller
+	// at stages of the cluster lifecycle.
+	HealthStatus string
 }
 
 // Snapshot represents a snapshot of a Kubernetes cluster object
@@ -31,7 +51,7 @@ type AwsCredentials struct {
 
 // NewCluster creates a new Cluster object and populates its
 // fields with the values from the Kubernetes cluster in the client passed to it.
-func NewCluster(c *client.Client) (*Cluster, error) {
+func NewCluster(c *client.KubeClient) (*Cluster, error) {
 	pods, err := getPods(c)
 	if err != nil {
 		return nil, err
@@ -83,7 +103,7 @@ func NewAwsCreds(region string) (*AwsCredentials, error) {
 
 // RefreshStatus performs a value overwrite of the cluster status.
 // This is useful for when the cluster is being updated.
-func (c *Cluster) RefreshStatus(client *client.Client) (err error) {
+func (c *Cluster) RefreshStatus(client *client.KubeClient) (err error) {
 	c.Nodes, err = GetAllNodes(client)
 	if err != nil {
 		return err
