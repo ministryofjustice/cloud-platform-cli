4692fca8e4172ee83bb0990f8d346b7c869dba3b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-10-05 14:07:55 +0100

    refactor(terraform): change comment casing to reflect function name

--- pkg/terraform/utils.go
+++ pkg/terraform/utils.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-// redacted reads bytes of data for any sensitive strings and print REDACTED
+// Redacted reads bytes of data for any sensitive strings and print REDACTED
 func Redacted(w io.Writer, output string, redact bool) {
 	re := regexp.MustCompile(`(?i)password|secret|token|key|https://hooks\.slack\.com|user|arn|ssh-rsa|clientid`)
 	scanner := bufio.NewScanner(strings.NewReader(output))
