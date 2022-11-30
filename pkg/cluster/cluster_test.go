747bc5cd9eace6c743de983651840a62e98afa83
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-11-30 15:45:12 +0000

    refactor(cluster): move functions to a shared file

--- pkg/cluster/cluster_test.go
+++ pkg/cluster/cluster_test.go
@@ -1,12 +1,29 @@
 package cluster
 
 import (
+	"encoding/base64"
+	"errors"
+	"os"
 	"reflect"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/aws/aws-sdk-go/service/eks/eksiface"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 )
+
+type mockEKSClient struct {
+	eksiface.EKSAPI
+}
+
+type mockEC2Client struct {
+	ec2iface.EC2API
+}
 
 func TestCluster_NewSnapshot(t *testing.T) {
 	tests := []struct {
@@ -91,5 +108,178 @@ func TestNewAwsCreds(t *testing.T) {
 				return
 			}
 		})
+	}
+}
+
+func (m *mockEC2Client) DescribeVpcs(input *ec2.DescribeVpcsInput) (*ec2.DescribeVpcsOutput, error) {
+	if *input.Filters[0].Values[0] != "test" {
+		return nil, errors.New("bad vpc")
+	}
+
+	return &ec2.DescribeVpcsOutput{
+		Vpcs: []*ec2.Vpc{
+			{
+				VpcId: aws.String("test"),
+			},
+		},
+	}, nil
+}
+
+func TestWriteKubeConfig(t *testing.T) {
+	cluster := &eks.Cluster{
+		Name:     aws.String("test"),
+		Endpoint: aws.String("https://test"),
+		CertificateAuthority: &eks.Certificate{
+			Data: aws.String("test"),
+		},
+	}
+
+	toks := token.Token{
+		Token: "test",
+	}
+
+	ca, err := base64.StdEncoding.DecodeString(aws.StringValue(cluster.CertificateAuthority.Data))
+	if err != nil {
+		t.Errorf("error decoding certificate: %v", err)
+	}
+
+	err = writeKubeConfig(cluster, "config.yaml", "aws", toks, ca)
+	if err != nil {
+		t.Errorf("WriteKubeConfig() error = %v", err)
+	}
+
+	defer func() {
+		err := os.Remove("config.yaml")
+		if err != nil {
+			t.Errorf("error removing config file: %v", err)
+		}
+	}()
+
+	if _, err := os.Stat("config.yaml"); os.IsNotExist(err) {
+		t.Errorf("WriteKubeConfig() error = %v", "config.yaml not found")
+	}
+
+	// Expect the test to fail from here.
+	err = writeKubeConfig(cluster, "", "aws", toks, []byte{})
+	if err == nil {
+		t.Errorf("WriteKubeConfig() error = %v", "expected error")
+	}
+
+	err = writeKubeConfig(cluster, "badPath", "aws", toks, nil)
+	if err == nil {
+		t.Errorf("WriteKubeConfig() error = %v", "expected error")
+	}
+
+	cluster.Name = nil
+	err = writeKubeConfig(cluster, "badPath", "aws", toks, nil)
+	if err == nil {
+		t.Errorf("WriteKubeConfig() error = %v", "expected error")
+	}
+}
+
+func TestNewClientset(t *testing.T) {
+	cluster := &eks.Cluster{
+		Name:     aws.String("test"),
+		Endpoint: aws.String("https://test"),
+		CertificateAuthority: &eks.Certificate{
+			Data: aws.String("test"),
+		},
+	}
+
+	_, err := newClientset(cluster, "aws", "test")
+	if err == nil {
+		t.Errorf("newClientset() error = %v", err)
+	}
+}
+
+func (m *mockEKSClient) DescribeCluster(input *eks.DescribeClusterInput) (*eks.DescribeClusterOutput, error) {
+	if *input.Name != "test" {
+		return nil, errors.New("cluster not found")
+	}
+
+	return &eks.DescribeClusterOutput{
+		Cluster: &eks.Cluster{
+			Name:   aws.String("test"),
+			Status: aws.String("ACTIVE"),
+		},
+	}, nil
+}
+
+func TestCheckVpc(t *testing.T) {
+	mockSvc := &mockEC2Client{}
+
+	testOpt := "test"
+
+	// Good path
+	err := checkVpc(testOpt, "test", mockSvc)
+	if err != nil {
+		t.Errorf("checkVpc() error = %v", err)
+	}
+
+	// Bad path
+	err = checkVpc(testOpt, "obviouslyWrong", mockSvc)
+	if err == nil {
+		t.Errorf("checkVpc() error = %v", err)
+	}
+
+	testOpt = "incorrectWorkspace"
+	err = checkVpc(testOpt, "test", mockSvc)
+	if err == nil {
+		t.Error("we expected an error here checkVpc() error")
+	}
+}
+
+func TestGetVpc(t *testing.T) {
+	mockSvc := &mockEC2Client{}
+
+	out, err := getVpc("test", mockSvc)
+	if err != nil {
+		t.Errorf("checkVpc() error = %v", err)
+	}
+
+	if *out.Vpcs[0].VpcId != "test" {
+		t.Errorf("checkVpc() error = %v", "vpc not found")
+	}
+
+	_, err = getVpc("bad", mockSvc)
+	if err == nil {
+		t.Errorf("was expecting an error. checkVpc() error = %v", "expected error")
+	}
+
+}
+
+func TestCheckCluster(t *testing.T) {
+	mockSvc := &mockEKSClient{}
+
+	// Good path
+	err := checkCluster("test", mockSvc)
+	if err != nil {
+		t.Errorf("checkCluster() error = %v", err)
+	}
+
+	// Bad path
+	err = checkCluster("bad", mockSvc)
+	if err == nil {
+		t.Errorf("checkCluster() error = %v", "expected error")
+	}
+}
+
+func TestGetCluster(t *testing.T) {
+	mockSvc := &mockEKSClient{}
+
+	// Good path
+	mockCluster, err := getCluster("test", mockSvc)
+	if err != nil {
+		t.Errorf("getCluster() error = %v", err)
+	}
+
+	if *mockCluster.Name != "test" {
+		t.Errorf("getCluster() error = %v", "cluster name not set")
+	}
+
+	// Bad path
+	_, err = getCluster("bad", mockSvc)
+	if err == nil {
+		t.Errorf("was expecting an error here. getCluster() error = %v", "expected error")
 	}
 }
--- pkg/cluster/create.go
+++ pkg/cluster/create.go
@@ -2,28 +2,18 @@ package cluster
 
 import (
 	"context"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
-	"github.com/aws/aws-sdk-go/service/eks"
-	"github.com/aws/aws-sdk-go/service/eks/eksiface"
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/ministryofjustice/cloud-platform-cli/pkg/client"
 	"github.com/ministryofjustice/cloud-platform-cli/pkg/terraform"
 	kubeErr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/clientcmd/api"
-	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 )
 
 // ApplyVpc when executed will Apply terraform code to create a Cloud Platform VPC and ensure it is up and running.
@@ -141,123 +131,6 @@ func terraformApply(tf *terraform.TerraformCLIConfig) (map[string]tfexec.OutputM
 	return terraform.Output(context.Background(), nil)
 }
 
-// AuthToCluster will authenticate to the cluster and return a kubernetes clientset. It will also write the kubeconfig
-// and set the current context to the eks cluster passed to it.
-func AuthToCluster(name string, eksSvc eksiface.EKSAPI, path string, awsProfile string) (*kubernetes.Clientset, error) {
-	input := &eks.DescribeClusterInput{
-		Name: aws.String(name),
-	}
-	result, err := eksSvc.DescribeCluster(input)
-	if err != nil {
-		log.Fatalf("Error calling DescribeCluster: %v", err)
-	}
-
-	clientset, err := newClientset(result.Cluster, path, awsProfile)
-	if err != nil {
-		log.Fatalf("Error creating clientset: %v", err)
-	}
-	return clientset, nil
-}
-
-func newClientset(cluster *eks.Cluster, path, awsProfile string) (*kubernetes.Clientset, error) {
-	gen, err := token.NewGenerator(true, false)
-	if err != nil {
-		return nil, err
-	}
-	opts := &token.GetTokenOptions{
-		ClusterID: aws.StringValue(cluster.Name),
-	}
-	tok, err := gen.GetWithOptions(opts)
-	if err != nil {
-		return nil, err
-	}
-	ca, err := base64.StdEncoding.DecodeString(aws.StringValue(cluster.CertificateAuthority.Data))
-	if err != nil {
-		return nil, err
-	}
-
-	config := &rest.Config{
-		Host:        aws.StringValue(cluster.Endpoint),
-		BearerToken: tok.Token,
-		TLSClientConfig: rest.TLSClientConfig{
-			CAData: ca,
-		},
-	}
-
-	if err := writeKubeConfig(cluster, path, awsProfile, tok, ca); err != nil {
-		return nil, err
-	}
-
-	return kubernetes.NewForConfig(config)
-}
-
-func writeKubeConfig(cluster *eks.Cluster, path, profile string, tok token.Token, ca []byte) error {
-	if path == "" {
-		return errors.New("kubeconfig path is empty")
-	}
-
-	if ca == nil {
-		return errors.New("ca is empty")
-	}
-
-	kc := api.Config{
-		Clusters: map[string]*api.Cluster{
-			*cluster.Name: {
-				Server:                   *cluster.Endpoint,
-				CertificateAuthorityData: ca,
-			},
-		},
-		Contexts: map[string]*api.Context{
-			*cluster.Name: {
-				Cluster:  *cluster.Name,
-				AuthInfo: *cluster.Name,
-			},
-		},
-		AuthInfos: map[string]*api.AuthInfo{
-			*cluster.Name: {
-				Token: tok.Token,
-			},
-		},
-		CurrentContext: *cluster.Name,
-	}
-
-	// write kubeconfig to disk
-	return clientcmd.WriteToFile(kc, path)
-}
-
-// CheckVpc asserts that the vpc is up and running. It tests the vpc state and id.
-func checkVpc(name, vpcId string, svc ec2iface.EC2API) error {
-	vpc, err := getVpc(name, svc)
-	if err != nil {
-		return fmt.Errorf("error describing vpc: %v", err)
-	}
-
-	if len(vpc.Vpcs) == 0 {
-		return fmt.Errorf("no vpc found")
-	}
-
-	if vpc.Vpcs[0].VpcId != nil && *vpc.Vpcs[0].VpcId != vpcId {
-		return fmt.Errorf("vpc id mismatch: %s != %s", *vpc.Vpcs[0].VpcId, vpcId)
-	}
-
-	if vpc.Vpcs[0].State != nil && *vpc.Vpcs[0].State != "available" {
-		return fmt.Errorf("vpc not available: %s", *vpc.Vpcs[0].State)
-	}
-
-	return nil
-}
-
-func getVpc(name string, svc ec2iface.EC2API) (*ec2.DescribeVpcsOutput, error) {
-	return svc.DescribeVpcs(&ec2.DescribeVpcsInput{
-		Filters: []*ec2.Filter{
-			{
-				Name:   aws.String("tag:Cluster"),
-				Values: []*string{aws.String(name)},
-			},
-		},
-	})
-}
-
 // applyTacticalPspFix deletes the current eks.privileged psp in the cluster.
 // This allows the cluster to be created with a different psp. All pods are recycled
 // so the new psp will be applied.
@@ -288,31 +161,6 @@ func applyTacticalPspFix(clientset kubernetes.Interface) error {
 	}
 
 	return nil
-}
-
-// checkCluster checks the cluster is created and exists.
-func checkCluster(name string, eks eksiface.EKSAPI) error {
-	cluster, err := getCluster(name, eks)
-	if err != nil {
-		return err
-	}
-
-	if *cluster.Status != "ACTIVE" {
-		return fmt.Errorf("cluster is not active")
-	}
-
-	return nil
-}
-
-func getCluster(name string, svc eksiface.EKSAPI) (*eks.Cluster, error) {
-	cluster, err := svc.DescribeCluster(&eks.DescribeClusterInput{
-		Name: aws.String(name),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return cluster.Cluster, nil
 }
 
 func deleteLocalState(dir string, paths ...string) error {
