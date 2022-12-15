1d0f768f8976a8878a8ada0df7f352c5ffadf940
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-12-15 12:05:09 +0000

    Update test to accept engine as args

--- pkg/environment/templatesRds_test.go
+++ pkg/environment/templatesRds_test.go
@@ -8,15 +8,18 @@ import (
 )
 
 func TestCreatesRdsTfFile(t *testing.T) {
-	filename := "resources/rds.tf"
+	filename := "resources/rds-postgresql.tf"
 	err := os.Mkdir("resources", 0o755)
 	if err != nil {
 		t.Error(err)
 	}
 
-	err = createRdsTfFile()
+	rdsFile, err := createRdsTfFile("postgresql")
 	if err != nil {
 		t.Errorf("Unexpected error: %s", err)
+	}
+	if rdsFile != filename {
+		t.Errorf("Expected file %s but got %s", filename, rdsFile)
 	}
 
 	moduleName := "github.com/ministryofjustice/cloud-platform-terraform-rds-instance"
