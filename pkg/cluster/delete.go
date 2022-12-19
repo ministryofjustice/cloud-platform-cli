b29067856ddbf82e628a0e9a819ec2c5b4934ee6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-12-19 21:51:40 +0000

    feat(cluster): add delete rough design outline

---
+++ pkg/cluster/delete.go
@@ -0,0 +1,22 @@
+package cluster
+
+// TODO: VERIFY
+// Verifies the cluster name isn't empty or forbidden.
+// Ensure workspace and kubeconfig cluster are the same name.
+// Ensure that the cluster is contactable.
+// Check env vars
+// Check AWS profiles are correct
+
+// TODO: TERRAFORM DESTROY
+// Destroy components in the cluster.
+// Check for removal.
+// Destroy the cluster.
+// Check for removal.
+// Destroy VPC and subnets, if not live
+// Check for removal.
+
+// TODO: VERIFY REMOVAL
+// Ensure that the cluster is no longer contactable.
+// Ensure that the cluster is no longer in the kubeconfig.
+// Ensure that the cluster is no longer in the workspace.
+// Ensure that the cluster is no longer in the state store.
