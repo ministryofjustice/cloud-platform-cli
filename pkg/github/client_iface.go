54f673a81653477acee89ffaf5f43769699e8810
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-01-18 13:36:48 +0000

    Use PR number for apply-namespace, add interface and mock tests

--- pkg/github/client_iface.go
+++ pkg/github/client_iface.go
@@ -12,4 +12,5 @@ import (
 type GithubIface interface {
 	ListMergedPRs(date util.Date, count int) ([]Nodes, error)
 	GetChangedFiles(int) ([]*github.CommitFile, error)
+	IsMerged(prNumber int) (bool, error)
 }
