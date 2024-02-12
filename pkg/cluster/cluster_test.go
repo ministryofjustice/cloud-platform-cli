e00791ee21a6c0190f094dbfcc1900ae25043424
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-02-12 15:06:37 +0000

    Remove getClusterName test

--- pkg/cluster/cluster_test.go
+++ pkg/cluster/cluster_test.go
@@ -3,9 +3,6 @@ package cluster
 import (
 	"reflect"
 	"testing"
-
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCluster_NewSnapshot(t *testing.T) {
@@ -26,41 +23,6 @@ func TestCluster_NewSnapshot(t *testing.T) {
 			got := c.NewSnapshot()
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Cluster.NewSnapshot() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_getClusterName(t *testing.T) {
-	type args struct {
-		nodes []v1.Node
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "getClusterName",
-			args: args{
-				nodes: []v1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node1",
-							Labels: map[string]string{
-								"Cluster": "test",
-							},
-						},
-					},
-				},
-			},
-			want: "test",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getClusterName(tt.args.nodes); got != tt.want {
-				t.Errorf("getClusterName() = %v, want %v", got, tt.want)
 			}
 		})
 	}
