cb15de6d7cd750f97c3509a1be91f24e9c047b48
sj-williams <steve.williams@digital.justice.gov.uk>
2024-03-12 10:01:37 +0000

    add optional branch flag to pipeline delete command

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -19,9 +19,9 @@ func runCmd(cliCmd string, args []string) {
 	}
 }
 
-func DeletePipelineShellCmds(clusterName string) {
+func DeletePipelineShellCmds(clusterName string, branchName string) {
 	runCmd("fly", []string{"--target", "manager", "login", "--team-name", "main", "--concourse-url", "https://concourse.cloud-platform.service.justice.gov.uk/"})
-	runCmd("bash", []string{"-c", "wget -qO- https://raw.githubusercontent.com/ministryofjustice/cloud-platform-terraform-concourse/main/pipelines/manager/main/delete-cluster.yaml | fly --target manager set-pipeline -p delete-cluster -c - -v cluster_name=" + clusterName})
+	runCmd("bash", []string{"-c", "wget -qO- https://raw.githubusercontent.com/ministryofjustice/cloud-platform-terraform-concourse/main/pipelines/manager/main/delete-cluster.yaml | fly --target manager set-pipeline -p delete-cluster -c - -v cluster_name=" + clusterName + " -v branch_name=" + branchName})
 	runCmd("fly", []string{"--target", "manager", "trigger-job", "-j", "delete-cluster/delete"})
 	fmt.Println("Deleting... https://concourse.cloud-platform.service.justice.gov.uk/teams/main/pipelines/delete-cluster/jobs/delete/builds/latest")
 }
