22947fa6d497d331c2b6c9f7376c838a6138cfbe
Mike Bell <mike.bell1@digital.justice.gov.uk>
2025-01-07 08:52:02 +0000

    feat: add create-custom-cluster and test-custom-cluster

--- pkg/commands/pipeline.go
+++ pkg/commands/pipeline.go
@@ -74,7 +74,7 @@ func addPipelineCordonAndDrainClusterCmd(toplevel *cobra.Command) {
 	toplevel.AddCommand(cmd)
 }
 
-func (opt *pipelineOptions) addPipelineDeleteClusterFlags(cmd *cobra.Command) {
+func (opt *pipelineOptions) addGenericClusterFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&cliOpt.Name, "cluster-name", "", "cluster to delete")
 	cmd.Flags().StringVarP(&cliOpt.BranchName, "branch-name", "b", "main", "branch name to use for pipeline run (default: main)")
 }
@@ -118,7 +118,81 @@ func addPipelineDeleteClusterCmd(toplevel *cobra.Command) {
 		},
 	}
 
-	cliOpt.addPipelineDeleteClusterFlags(cmd)
+	cliOpt.addGenericClusterFlags(cmd)
+	toplevel.AddCommand(cmd)
+}
+
+func (opt *pipelineOptions) addCreateCustomPipelineClusterFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&cliOpt.BranchName, "branch-name", "b", "main", "branch name to use for pipeline run (default: main)")
+}
+
+func addCreateCustomClusterCmd(toplevel *cobra.Command) {
+	cmd := &cobra.Command{
+		Use:   "create-custom-cluster",
+		Short: `create a custom cloud-platform cluster via the pipeline`,
+		Long: heredoc.Doc(`
+
+			Running this command will create a custom eks cluster in the cloud-platform aws account.
+
+      This command will run remotely in the pipeline
+
+			Optionally you can pass a branch name to use for the pipeline run, default is "main"
+
+      ** You _must_ have the fly cli installed **
+      --> https://concourse-ci.org/fly.html
+
+      ** You must also have wget installed **
+      --> brew install wget
+`),
+		PreRun: upgradeIfNotLatest,
+		Run: func(cmd *cobra.Command, args []string) {
+			pipeline.CreateCustomPipelineShellCmds(cliOpt.BranchName)
+		},
+	}
+
+	cliOpt.addCreateCustomPipelineClusterFlags(cmd)
+	toplevel.AddCommand(cmd)
+}
+
+func addTestCustomClusterCmd(toplevel *cobra.Command) {
+	cmd := &cobra.Command{
+		Use:   "test-custom-cluster",
+		Short: `test a custom cloud-platform cluster via the pipeline`,
+		Long: heredoc.Doc(`
+
+			Running this command will test a custom eks cluster in the cloud-platform aws account.
+
+      The delete will run remotely in the pipeline
+
+			You must have the following environment variables set, or passed via arguments:
+				- a cluster name
+
+			Optionally you can pass a branch name to use for the pipeline run, default is "main"
+
+      ** You _must_ have the fly cli installed **
+      --> https://concourse-ci.org/fly.html
+
+      ** You must also have wget installed **
+      --> brew install wget
+`),
+		PreRun: upgradeIfNotLatest,
+		Run: func(cmd *cobra.Command, args []string) {
+			contextLogger := log.WithFields(log.Fields{"subcommand": "test-custom-cluster"})
+			cliOpt.MaxNameLength = 12
+
+			if cliOpt.Name == "" {
+				contextLogger.Fatal("--cluster-name is required")
+			}
+
+			if err := cliOpt.IsNameValid(); err != nil {
+				contextLogger.Fatal(err)
+			}
+
+			pipeline.TestCustomPipelineShellCmds(cliOpt.Name, cliOpt.BranchName)
+		},
+	}
+
+	cliOpt.addGenericClusterFlags(cmd)
 	toplevel.AddCommand(cmd)
 }
 
@@ -127,6 +201,8 @@ func addPipelineCmd(topLevel *cobra.Command) {
 
 	addPipelineDeleteClusterCmd(pipelineCmd)
 	addPipelineCordonAndDrainClusterCmd(pipelineCmd)
+	addCreateCustomClusterCmd(pipelineCmd)
+	addTestCustomClusterCmd(pipelineCmd)
 }
 
 var pipelineCmd = &cobra.Command{
