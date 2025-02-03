f238879a2b9a8628bce0f2785ddb475a53654b5a
sj-williams <steve.williams@digital.justice.gov.uk>
2025-02-03 15:57:52 +0000

    style: 💄 make our namespace apply fail bot reminder kinder

--- pkg/slack/post.go
+++ pkg/slack/post.go
@@ -8,7 +8,7 @@ import (
 
 func post(user, ts, webhookUrl, buildUrl string) error {
 	// https://pkg.go.dev/github.com/slack-go/slack#PostWebhook
-	message := fmt.Sprintf("<@%s> <%s|your build failed>, please address immediately or add a <https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/long-running-env-operations.html|APPLY_PIPELINE_SKIP_THIS_NAMESPACE> to your namespace to prevent our pipelines from being blocked", user, buildUrl)
+	message := fmt.Sprintf("<@%s> <%s|your build failed>, please review and resolve issues, or add an <https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/concourse-pipelines.html#apply-live-pipelines-and-apply-pipeline-skip-this-namespace|APPLY_PIPELINE_SKIP_THIS_NAMESPACE> to your namespace.", user, buildUrl)
 
 	webhookMsg := slack.WebhookMessage{
 		Channel:         "ask-cloud-platform",
