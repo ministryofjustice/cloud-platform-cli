134791844ce093925e1d270576f70e7e7f127c84
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2025-03-28 13:18:40 +0000

    fixup! style: 💄 open the tf summary by default (increase visibility)

--- pkg/environment/createPlanSummary_test.go
+++ pkg/environment/createPlanSummary_test.go
@@ -38,7 +38,7 @@ func Test_CreateCommentBody(t *testing.T) {
 	changesOutput := `
 <h1>Terraform Plan Summary</h1>
 
-<details "open">
+<details open>
 	<summary>
 		<b>Terraform Plan: %d to be created, %d to be destroyed, %d to be updated, %d to be replaced and %d unchanged.</b>
 	</summary>
