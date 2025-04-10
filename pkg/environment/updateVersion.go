00180cb7a4d3ea631bc038436a9101b19a3cdff1
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-04-10 21:24:53 +0100

    fix: wip updateVersion refactor

--- pkg/environment/updateVersion.go
+++ pkg/environment/updateVersion.go
@@ -2,6 +2,7 @@ package environment
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -10,61 +11,91 @@ import (
 )
 
 func updateVersion(moduleName, actualDbVersion, terraformDbVersion, tfDir string) ([]string, error) {
+	log.Printf("Checking if directory exists: %s", tfDir)
 	if _, err := os.Stat(tfDir); os.IsNotExist(err) {
 		abs, _ := filepath.Abs(tfDir)
 		return nil, fmt.Errorf("directory does not exist: %s (resolved to: %s)", tfDir, abs)
 	}
 
-	grepModule := exec.Command("/bin/sh", "-c", "grep -lr 'module \""+moduleName+"\"' *")
-	grepModule.Dir = tfDir
-	moduleFileBytes, err := grepModule.Output()
+	grepCmd := fmt.Sprintf("grep -l 'module \"%s\"' *.tf", moduleName)
+	cmd := exec.Command("/bin/sh", "-c", grepCmd)
+	cmd.Dir = tfDir
+	output, err := cmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("failed to grep module reference: %w", err)
+		return nil, fmt.Errorf("failed to find module file: %w", err)
 	}
-	files := strings.Split(strings.TrimSpace(string(moduleFileBytes)), "\n")
-	if len(files) == 0 {
-		return nil, fmt.Errorf("no files found referencing module %s", moduleName)
-	}
-	fileName := files[0]
-	changedFiles := []string{fileName}
+	fileName := strings.TrimSpace(strings.Split(string(output), "\n")[0])
 
-	sedCmd := fmt.Sprintf("sed -i -e 's/db_engine_version *= *\"%s\"/db_engine_version = \"%s\"/g' %s", terraformDbVersion, actualDbVersion, fileName)
-	hardcodedSed := exec.Command("/bin/sh", "-c", sedCmd)
-	hardcodedSed.Dir = tfDir
-	_ = hardcodedSed.Run()
+	var changedFiles []string
 
-	scanModuleBlock := exec.Command("/bin/sh", "-c", "grep -A 40 'module \""+moduleName+"\"' "+fileName)
-	scanModuleBlock.Dir = tfDir
-	scanOut, err := scanModuleBlock.Output()
+	log.Printf("Updating hardcoded db_engine_version in file: %s", fileName)
+	hardcodeUpdated, err := updateVersionHardcode(fileName, actualDbVersion, terraformDbVersion, tfDir)
 	if err != nil {
-		return nil, fmt.Errorf("failed to scan module block: %w", err)
+		return nil, err
+	}
+	if hardcodeUpdated {
+		changedFiles = append(changedFiles, fileName)
 	}
 
-	re := regexp.MustCompile(`(?i)db_engine_version *= *var\.([a-zA-Z0-9_\-]+)`)
-	match := re.FindStringSubmatch(string(scanOut))
-	if len(match) < 2 {
-		return changedFiles, nil
-	}
-	varName := match[1]
-
-	grepVar := exec.Command("/bin/sh", "-c", "grep -l 'variable \""+varName+"\"' *")
-	grepVar.Dir = tfDir
-	varFileBytes, err := grepVar.Output()
+	log.Printf("Updating variable db_engine_version in file: %s", fileName)
+	varUpdated, varFile, err := updateVersionVariable(fileName, moduleName, actualDbVersion, terraformDbVersion, tfDir)
 	if err != nil {
-		return nil, fmt.Errorf("could not find variable definition for %s: %v", varName, err)
+		return nil, err
 	}
-	varFile := strings.TrimSpace(string(varFileBytes))
-
-	varSedCmd := fmt.Sprintf("sed -i -e 's/default = \"%s\"/default = \"%s\"/' %s", terraformDbVersion, actualDbVersion, varFile)
-	replaceDefault := exec.Command("/bin/sh", "-c", varSedCmd)
-	replaceDefault.Dir = tfDir
-	if err := replaceDefault.Run(); err != nil {
-		return nil, fmt.Errorf("failed to update variable default: %v", err)
-	}
-
-	if !contains(changedFiles, varFile) {
+	if varUpdated && varFile != "" && varFile != fileName {
 		changedFiles = append(changedFiles, varFile)
 	}
 
+	if len(changedFiles) == 0 {
+		return nil, fmt.Errorf("no changes applied for module: %s", moduleName)
+	}
+
 	return changedFiles, nil
+}
+
+func updateVersionHardcode(fileName, actualDbVersion, terraformDbVersion, tfDir string) (bool, error) {
+	sedCmd := fmt.Sprintf("sed -i -e 's/db_engine_version *= *\"%s\"/db_engine_version = \"%s\"/g' %s", terraformDbVersion, actualDbVersion, fileName)
+	cmd := exec.Command("/bin/sh", "-c", sedCmd)
+	cmd.Dir = tfDir
+	if err := cmd.Run(); err != nil {
+		return false, fmt.Errorf("failed to update hardcoded version: %w", err)
+	}
+	log.Printf("Hardcoded db_engine_version updated successfully in %s", fileName)
+	return true, nil
+}
+
+func updateVersionVariable(fileName, moduleName, actualDbVersion, terraformDbVersion, tfDir string) (bool, string, error) {
+	scanCmd := fmt.Sprintf("grep -A 40 'module \"%s\"' %s", moduleName, fileName)
+	cmd := exec.Command("/bin/sh", "-c", scanCmd)
+	cmd.Dir = tfDir
+	output, err := cmd.Output()
+	if err != nil {
+		return false, "", fmt.Errorf("failed to scan module block: %w", err)
+	}
+
+	re := regexp.MustCompile(`(?i)db_engine_version *= *var\.([a-zA-Z0-9_\-]+)`)
+	matches := re.FindStringSubmatch(string(output))
+	if len(matches) < 2 {
+		log.Printf("No variable found for db_engine_version in module %s", moduleName)
+		return false, "", nil
+	}
+	varName := matches[1]
+
+	grepVarCmd := fmt.Sprintf("grep -l 'variable \"%s\"' *.tf", varName)
+	varCmd := exec.Command("/bin/sh", "-c", grepVarCmd)
+	varCmd.Dir = tfDir
+	varOutput, err := varCmd.Output()
+	if err != nil {
+		return false, "", fmt.Errorf("failed to find variable definition for %s: %w", varName, err)
+	}
+	varFile := strings.TrimSpace(string(varOutput))
+
+	varSedCmd := fmt.Sprintf("sed -i -e 's/default = \"%s\"/default = \"%s\"/' %s", terraformDbVersion, actualDbVersion, varFile)
+	updateVarCmd := exec.Command("/bin/sh", "-c", varSedCmd)
+	updateVarCmd.Dir = tfDir
+	if err := updateVarCmd.Run(); err != nil {
+		return false, "", fmt.Errorf("failed to update variable default: %w", err)
+	}
+	log.Printf("Variable db_engine_version updated successfully in %s", varFile)
+	return true, varFile, nil
 }
