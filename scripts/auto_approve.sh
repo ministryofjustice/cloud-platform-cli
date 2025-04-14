fc89d1b7b878246d26efe59851ec4e21bce80f5c
Mike Bell <mike.bell1@digital.justice.gov.uk>
2025-04-14 10:08:00 +0100

    feat: update script to work with new folder layout

--- scripts/auto_approve.sh
+++ scripts/auto_approve.sh
@@ -9,8 +9,16 @@ JSON_FILE="${PLAN_NAME%.out}.json"
 
 terraform -chdir="$PLAN_DIR" show -json "$PLAN_NAME" > "$JSON_FILE"
 
-OUTPUT=$(opa exec --decision terraform/analysis/allow --bundle opa-auto-approve-policy/ "$JSON_FILE")
-OPA_RESULT=$(echo "$OUTPUT" | jq -r '.result[0].result')
+results=()
+
+for dir in opa-auto-approve-policy/*/;
+do
+    OUTPUT=$(opa exec --decision terraform/analysis/allow --bundle $dir "$JSON_FILE")
+    OPA_RESULT=$(echo "$OUTPUT" | jq -r '.result[0].result')
+    results+=($dir","$OPA_RESULT)
+done
+
+# test=("opa-auto-approve-policy/ecr/,true" "opa-auto-approve-policy/service_pod/,true")
 
 CHANGED_FILES=$(curl -L \
     -H "Accept: application/vnd.github+json" \
@@ -33,40 +41,49 @@ for f in $CHANGED_FILES; do
     fi
 done
 
-if [ "$OPA_RESULT" = true ] && [ "$YAML_CHANGES" -eq 0 ]; then
-    curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/ministryofjustice/cloud-platform-environments/pulls/$PR/reviews" \
-    -d '{
-        "body": ":white_check_mark: **Auto-Approved!**\n\nThis PR has **passed the [OPA auto approve policy](https://github.com/ministryofjustice/cloud-platform-environments/tree/main/opa-auto-approve-policy) check and security validation**.\n\nYou can merge whenever suits you! :rocket:",
-        "event": "APPROVE"
-    }'
+if [[ ${results[@]} =~ "false" ]] && [ "$YAML_CHANGES" -eq 0 ];
+then
+  REASON=":male_detective: **Manual review required: [OPA auto approve policy](https://github.com/ministryofjustice/cloud-platform-environments/tree/main/opa-auto-approve-policy) checks did not pass.**"
+
+  string="\n| Test | Passed? |\n| --- | --- |\n|"
+  for t in ${result[@]}; do
+    for th in $(echo $t | tr "," "\n")
+    do
+      string+=" $th |"
+    done
+    string+="\n"
+  done
+
+  REASON+=$string
+
+  if [ "$YAML_CHANGES" -ne 0 ]; then
+      if [ -n "$REASON" ]; then
+          REASON="$REASON\n:male_detective: **Detected changes to K8s YAML files. Manual review needed.**"
+      else
+          REASON=":male_detective: **Detected changes to K8s YAML files. Manual review needed.**"
+      fi
+  fi
+
+  curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/ministryofjustice/cloud-platform-environments/issues/$PR/comments" \
+  -d '{
+      "body": "This PR **CANNOT** be auto approved and requires manual approval from the Cloud Platform team.\n Reason:\n '"$REASON"'\n Please raise it in #ask-cloud-platform Slack channel."
+  }'
 else
-    REASON=""
-    if [ "$OPA_RESULT" != true ]; then
-        REASON=":male_detective: **Manual review required: [OPA auto approve policy](https://github.com/ministryofjustice/cloud-platform-environments/tree/main/opa-auto-approve-policy) checks did not pass.**"
-    fi
-
-    if [ "$YAML_CHANGES" -ne 0 ]; then
-        if [ -n "$REASON" ]; then
-            REASON="$REASON\n:male_detective: **Detected changes to K8s YAML files. Manual review needed.**"
-        else
-            REASON=":male_detective: **Detected changes to K8s YAML files. Manual review needed.**"
-        fi
-    fi
-
-    curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/ministryofjustice/cloud-platform-environments/issues/$PR/comments" \
-    -d '{
-        "body": "This PR **CANNOT** be auto approved and requires manual approval from the Cloud Platform team.\n Reason:\n '"$REASON"'\n Please raise it in #ask-cloud-platform Slack channel."
-    }'
+  curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/ministryofjustice/cloud-platform-environments/pulls/$PR/reviews" \
+  -d '{
+    "body": ":white_check_mark: **Auto-Approved!**\n\nThis PR has **passed the [OPA auto approve policy](https://github.com/ministryofjustice/cloud-platform-environments/tree/main/opa-auto-approve-policy) check and security validation**.\n\nYou can merge whenever suits you! :rocket:",
+    "event": "APPROVE"
+  }'
 fi
 
 exit 0
